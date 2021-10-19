c0dc9993a5837c51d1160ab7f0a607e99815233a
Abdullah Al Shabk <89540228+Abdushabk@users.noreply.github.com>
2021-10-19 16:25:06 +0200

    last change

--- 2app.js
+++ 2app.js
@@ -4,7 +4,7 @@ const addButton = document.querySelector('.addButton');
 const removeButton = document.querySelector('.removeButton');
 const newItem = document.querySelector('.newItem');
 const listContent = document.querySelector('.listContent');
- 
+
 
 console.log(addButton);
 console.log(removeButton);
@@ -13,12 +13,12 @@ console.log(listContent);
 
 //EVENT LISTENERS
 addButton.addEventListener( 'click', addingItems);
-removeButton.addEventListener('click', deletingItem);
+
 
 //FUNCTIONS
 
 function addingItems() {
-    //console.log(newItem.value);
+    console.log(newItem.value);
     const toDoLi = document.createElement('li');
     const toDoInput = document.createElement('input');
     toDoLi.innerText = newItem.value
@@ -30,23 +30,5 @@ function addingItems() {
     listContent.appendChild(toDoLi);
     toDoLi.appendChild(toDoInput);
     toDoLi.innerHTML = `<input class="form-check-input me-3" type="checkbox" value="" aria-label="..." />${newItem.value}`
-    //the not yet existing element being grabbed
-    const  checkboxActive = document.querySelectorAll('.form-check-input');
-    checkboxActive.forEach(box => {box.addEventListener('click', selectingItem )
-    console.log(box);});
-    
 }
 
-function selectingItem (event) {
-   // newItem.classList.add('deleteMe');
-//console.log(event.target);
-//console.log(event.target.parentNode);
-event.target.parentNode.classList.toggle('deleteMe');
-event.target.parentNode.classList.toggle('crossOut');
-}
-
-function deletingItem () {
-    const  toBeDeleted = document.querySelectorAll('.crossOut'); 
-    toBeDeleted.forEach(box => {box.remove();
-        console.log(box);})
-}
