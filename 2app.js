a56153ce544398db78f59ffecab8543c5c88c679
Abdullah Al Shabk <89540228+Abdushabk@users.noreply.github.com>
2021-10-19 16:31:41 +0200

    Add files via upload

--- 2app.js
+++ 2app.js
@@ -13,12 +13,12 @@ console.log(listContent);
 
 //EVENT LISTENERS
 addButton.addEventListener( 'click', addingItems);
-
+removeButton.addEventListener('click', deletingItem);
 
 //FUNCTIONS
 
 function addingItems() {
-    console.log(newItem.value);
+    //console.log(newItem.value);
     const toDoLi = document.createElement('li');
     const toDoInput = document.createElement('input');
     toDoLi.innerText = newItem.value
@@ -29,6 +29,33 @@ function addingItems() {
     //toDoInput.classList.add("form-check-input", "me-3");
     listContent.appendChild(toDoLi);
     toDoLi.appendChild(toDoInput);
-    toDoLi.innerHTML = `<input class="form-check-input me-3" type="checkbox" value="" aria-label="..." />${newItem.value}`
+    toDoLi.innerHTML = `<input class="form-check-input me-3" type="checkbox" value="" aria-label="..." /> <div>${newItem.value}</div>`
+    //the not yet existing element being grabbed
+    const  checkboxActive = document.querySelectorAll('.form-check-input');
+    checkboxActive.forEach(box => {box.addEventListener('click', selectingItem )
+    console.log(box);});
+    const edit = document.querySelectorAll("li div")
+    edit.forEach(item => {item.addEventListener('dblclick', editItem)})
+    console.log(edit)
 }
 
+function selectingItem (event) {
+   // newItem.classList.add('deleteMe');
+//console.log(event.target);
+//console.log(event.target.parentNode);
+event.target.parentNode.classList.toggle('deleteMe');
+event.target.parentNode.classList.toggle('crossOut');
+}
+
+function deletingItem () {
+    const  toBeDeleted = document.querySelectorAll('.crossOut'); 
+    toBeDeleted.forEach(box => {box.remove();
+        console.log(box);})
+}
+
+function editItem (event) {
+    const inputField = document.createElement("input")
+    event.target.parentNode.appendChild(inputField)
+    event.target.remove()
+    console.log(event)
+}
